b4ad46bf572e7361d1dba3a8b37ea3c23749ddd5
kripa <kripa@MacBook.local>
2025-10-06 11:36:25 +0545

    folders

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,31 @@
+import React from 'react'
+import Navbar from './Component/Navbar'
+import Footer from './Component/Footer'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './Pages/Home'
+import Movies from './Pages/Movies'
+import MovieDetail from './Pages/MovieDetail'
+import SeatLayout from './Pages/SeatLayout'
+import Favorite from './Pages/Favorite'
+import MyBooking from './Pages/MyBooking'
+import { Toaster } from "react-hot-toast"
 
-import './App.css'
-
-function App() {
-
+const App = () => {
+  const IsAdminRoutes = useLocation().pathname.startsWith("/admin")
 
   return (
     <>
-      hello
+      <Toaster />
+      {!IsAdminRoutes && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetail />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/favorite" element={<Favorite />} />
+        <Route path="/mybooking" element={<MyBooking />} />
+      </Routes>
+      {!IsAdminRoutes && <Footer />}
     </>
   )
 }
---
+++ Frontend/src/Pages/Favorite.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Favorite = () => {
+  return (
+    <div>
+      Fav
+    </div>
+  )
+}
+
+export default Favorite
